9e29996dd5da7536bf9a616ac90f14bb9d312ca6
Nalla Prakash <hamhaunalla99@gmail>
2022-01-10 23:28:16 +0530

    idk something went wrong

--- project.py
+++ project.py
@@ -4,17 +4,12 @@ from kivymd.uix.label import MDLabel
 
 
 class CanteenApp(MDApp):
-
-    # Friendly mesazze from none other than the owner itself 😲
-    _owner_mesazze = "Jaldi Jaldi Khana Banao Tumhari Maka Chodon 😙"
+    
+    _owner_mesazze = "Jaldi Jaldi Khana Bamao Tumhari Maka Chodon 😙"
 
     def build(self):
-        # Theming
         self.theme_cls.theme_style = "Dark"  # Dark theme for app
-        self.theme_cls.primary_palette = "Purple"
-        self.theme_cls.primary_hue = "300"
-
-        # Initialise and add widget to screen
+        
         screen = MDScreen()
         screen.add_widget(
             MDLabel(
@@ -22,5 +17,5 @@ class CanteenApp(MDApp):
                 halign="center")
         )
 
-if __name__ == '__main__':
-    CanteenApp().run()
+
+CanteenApp().run()
